f150e2680abd374bd5f7efcf61af00684adb3ba5
Tatiana Mantrova <tatiana.mantrova@gmail.com>
2023-02-03 14:29:23 +0300

    сверстан макет первого экрана

--- LoginApp/LoginViewController.swift
+++ LoginApp/LoginViewController.swift
@@ -7,8 +7,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginViewController: UIViewController {
 
+    @IBOutlet var userNameTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
