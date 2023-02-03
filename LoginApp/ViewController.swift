94ba875e9abed9e38a63a057125331f30f433e4c
Tatiana Mantrova <tatiana.mantrova@gmail.com>
2023-02-03 12:11:54 +0300

    Initial Commit

---
+++ LoginApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LoginApp
+//
+//  Created by Tatiana Mantrova on 03.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
